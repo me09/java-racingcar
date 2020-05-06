bd894b61ec4bbe619ef1b82c86ddded7fad0532c
KNY <kny011598@gmail.com>
2020-05-06 13:39:05 +0900

    feat: 랜덤한 숫자에 따라 자동차 전진 또는 정지

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,8 @@
 package domain;
 
 public class Car {
+    private static final int RANDOM_RANGE = 10;
+    private static final int CONDITION_FORWARD_OR_NOT = 4;
     private final String name;
     private int position = 0;
 
@@ -8,5 +10,15 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void movePosition() {
+        int condition = (int)(Math.random() * RANDOM_RANGE);
+
+        if (condition >= CONDITION_FORWARD_OR_NOT) {
+            this.position += 1;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
 }
